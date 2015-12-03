498705135c427c02de17e4516910d342d4a755ca
Hugo Josefson <hugo@josefson.org>
2015-12-03 22:03:06 +0100

    01 gold

--- 01/gold.js
+++ 01/gold.js
@@ -5,4 +5,6 @@ export default input => input
     .filter(char => char === '(' || char === ')')
     .map(char => char === '(' ? 1 : char)
     .map(char => char === ')' ? -1 : char)
-    .reduce(0, (floor, direction) => floor + direction);
+    .scan(0, (floor, direction) => floor + direction)
+    .takeWhile(floor => floor > -1)
+    .reduce(0, (pos, floor) => pos + 1);
