6d72b45b382fd6749c538f3688c1006378e344bb
Hugo Josefson <hugo@josefson.org>
2015-12-04 00:00:41 +0100

    02 test

---
+++ 02/test.js
@@ -0,0 +1,15 @@
+import describeTestCases from '../lib/describe-test-cases';
+
+import silver from './silver';
+import gold from './gold';
+
+describe('02', () => {
+    describeTestCases('silver', silver, [
+        ['2x3x4', 58],
+        ['1x1x10', 43]
+    ]);
+    describeTestCases('gold', gold, [
+        ['2x3x4', 34],
+        ['1x1x10', 14]
+    ]);
+});
