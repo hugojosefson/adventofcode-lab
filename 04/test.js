2f88edad93d0f5ba3c53d909dd56a0678b9c3075
Hugo Josefson <hugo@josefson.org>
2015-12-04 21:17:48 +0100

    04 test silver

--- 04/test.js
+++ 04/test.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import describeTestCases from '../lib/describe-test-cases';
 
 import silver from './silver';
@@ -5,6 +7,7 @@ import silver from './silver';
 describe('04', () => {
     describeTestCases('silver', silver, [
         ['abcdef', 609043],
-        ['pqrstuv', 1048970]
+        ['pqrstuv', 1048970],
+        [fs.readFileSync(path.join(__dirname, 'input'), 'utf8'), 117946]
     ], 60000);
 });
