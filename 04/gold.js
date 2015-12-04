86a429736f9c0aeadcb010eb72cad0266142ca89
Hugo Josefson <hugo@josefson.org>
2015-12-04 21:21:36 +0100

    04 gold

---
+++ 04/gold.js
@@ -0,0 +1,3 @@
+import algorithm from './algorithm';
+
+export default algorithm(hash => hash.substring(0, 6) === '000000');
--- 04/test.js
+++ 04/test.js
@@ -3,6 +3,7 @@ import path from 'path';
 import describeTestCases from '../lib/describe-test-cases';
 
 import silver from './silver';
+import gold from './gold';
 
 describe('04', () => {
     describeTestCases('silver', silver, [
@@ -10,4 +11,8 @@ describe('04', () => {
         ['pqrstuv', 1048970],
         [fs.readFileSync(path.join(__dirname, 'input'), 'utf8'), 117946]
     ], 60000);
+
+    describeTestCases('gold', gold, [
+        [fs.readFileSync(path.join(__dirname, 'input'), 'utf8'), 3938038]
+    ], 600000);
 });
