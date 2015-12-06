5772f30cdd96e1122ee0d4ee7b35d4dba6860d84
Hugo Josefson <hugo@josefson.org>
2015-12-06 12:33:55 +0100

    06 clean up debug logging

--- 06/algorithm.js
+++ 06/algorithm.js
@@ -2,30 +2,9 @@ import Bacon from 'baconjs';
 import _ from 'lodash';
 import numericCompare from '../lib/numeric-compare';
 
-function error(msg, x1, y1, x2, y2, fromX, fromY, toX, toY) {
-    console.error(msg, [{x1, y1, x2, y2}, {fromX, fromY, toX, toY}]);
-    throw new Error(msg);
-}
-
 const operateOnLights = (lights, [operation, x1, y1, x2, y2]) => {
-    if (typeof operation !== 'function') {
-        throw new Error(`${operation} is not a function.`)
-    }
     const [fromX, toX] = [x1, x2].sort(numericCompare);
     const [fromY, toY] = [y1, y2].sort(numericCompare);
-
-    if (fromX !== x1) {
-        error('fromX !== x1', x1, y1, x2, y2, fromX, fromY, toX, toY);
-    }
-    if (toX !== x2) {
-        error('toX !== x2', x1, y1, x2, y2, fromX, fromY, toX, toY);
-    }
-    if (fromY !== y1) {
-        error('fromY !== y1', x1, y1, x2, y2, fromX, fromY, toX, toY);
-    }
-    if (toY !== y2) {
-        error('toY !== y2', x1, y1, x2, y2, fromX, fromY, toX, toY);
-    }
 
     for (let x = fromX; x <= toX; x++) {
         for (let y = fromY; y <= toY; y++) {
