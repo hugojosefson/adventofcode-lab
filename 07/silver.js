b4c9aed6e25de3e4b36b52826c53baf3160c09f0
Hugo Josefson <hugo@josefson.org>
2015-12-07 22:08:05 +0100

    07

---
+++ 07/silver.js
@@ -0,0 +1,25 @@
+import Bacon from 'baconjs';
+import _ from 'lodash';
+import _eval from 'eval';
+
+import jsExpression from './js-expression';
+
+export default input$ => input$
+    .flatMap(input => Bacon.fromArray(input.split('\n')))
+    .map(line => line.match(/^(.+?)->(.+)$/))
+    .filter(_.identity)
+    .map(matches => matches.splice(1))
+    .map(([expression, name]) => [expression.trim(), name.trim()])
+    .map(([expression, name]) => [jsExpression(expression), name])
+    .map(([jsExpression, name]) => `var _${name} = memoize(function() { return (${jsExpression}) & 0xffff;});`)
+    .reduce([], (array, statement) => {
+        array.push(statement);
+        return array;
+    })
+    .map(array => {
+        array.unshift(`var memoize = require('lodash').memoize;`);
+        array.push('module.exports = _a()');
+        return array;
+    })
+    .map(array => array.join('\n'))
+    .map(js => _eval(js, 'day07', {require}))
---
+++ 07/test.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import describeTestCases from '../lib/describe-test-cases';
+import describeSimpleTestCases from '../lib/describe-simple-test-cases';
+
+import jsExpression from './js-expression';
+import silver from './silver';
+
+describe.only('05', () => {
+    describeSimpleTestCases('js-expression', jsExpression, [
+        ['123', '123'],
+        ['x', '_x()'],
+        ['xy', '_xy()'],
+        ['x yd', '_x() _yd()'],
+        ['x AND y', '_x() & _y()'],
+        ['x OR y', '_x() | _y()'],
+        ['x RSHIFT 2', '_x() >> 2'],
+        ['x RSHIFT y', '_x() >> _y()'],
+        ['x LSHIFT 13', '_x() << 13'],
+        ['NOT x', '~ _x()']
+    ]);
+
+    describeTestCases('silver', silver, [
+        [fs.readFileSync(path.join(__dirname, 'input'), 'utf8'), 46065, 'input']
+    ], 2000);
+});
