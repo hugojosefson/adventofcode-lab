7f284e3048f816d683c6d30026add7bdbfc570c5
Hugo Josefson <hugo@josefson.org>
2015-12-08 22:08:55 +0100

    08 gold

---
+++ 08/gold.js
@@ -0,0 +1,15 @@
+import Bacon from 'baconjs';
+import _ from 'lodash';
+
+import sum from '../lib/sum';
+
+export default input$ => input$
+    .flatMap(input => Bacon.fromArray(input.split('\n')))
+    .filter(_.identity)
+    .map(line => [line.length, line])
+    .map(([originalLength, s]) => [originalLength, s.replace(/\\/g, '\\\\')])
+    .map(([originalLength, s]) => [originalLength, s.replace(/"/g, '\\"')])
+    .map(([originalLength, s]) => [originalLength, `"${s}"`])
+    .map(([originalLength, s]) => [originalLength, s.length])
+    .map(([originalLength, longerString]) => longerString - originalLength)
+    .reduce(0, sum)
--- 08/test.js
+++ 08/test.js
@@ -3,8 +3,9 @@ import path from 'path';
 import describeTestCases from '../lib/describe-test-cases';
 
 import silver from './silver';
+import gold from './gold';
 
-describe.only('05', () => {
+describe.only('08', () => {
     describeTestCases('silver', silver, [
         ['""', 2 - 0],
         ['"abc"', 5 - 3],
@@ -20,5 +21,21 @@ describe.only('05', () => {
             12
         ],
         [fs.readFileSync(path.join(__dirname, 'input'), 'utf8'), 1371, 'input']
-    ], 2000);
+    ]);
+
+    describeTestCases('gold', gold, [
+        ['""', 6-2],
+        ['"abc"', 9-5],
+        ['"aaa\\"aaa"', 16-10],
+        ['"\\x27"', 11-6],
+        [
+            [
+                '""',
+                '"abc"',
+                '"aaa\\"aaa"',
+                '"\\x27"'
+            ].join('\n'),
+            19
+        ]
+    ]);
 });
