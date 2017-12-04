4a110d2212c6093977f46fae31a0aeda562f998c
Hugo Josefson <hugo@josefson.org>
2017-12-04 16:56:31 +0100

    Revert 2017 day 3 silver to original fast impl.
    Re-enable input test :)

--- 2017/03/silver.js
+++ 2017/03/silver.js
@@ -1,13 +1,28 @@
 import Bacon from 'baconjs'
+const {sqrt, floor, pow} = Math
 
-import common from './common'
+export const widthOfSquare = n => (floor(sqrt(n) / 2 + 0.5) - 1) * 2 + 1
+export const maxInSquare = n => pow(widthOfSquare(n), 2)
+export const isMaxInSquare = n => n === maxInSquare(n)
+export const taxiDistanceOfMaxInSquare = n => widthOfSquare(n) - 1
+export const snakeLengthSinceSquareCorner = n => n - maxInSquare(n)
+export const snakeLengthSinceAnyCorner = n => snakeLengthSinceSquareCorner(n) % (widthOfSquare(n) + 1)
+export const middleOfSquare = n => (widthOfSquare(n) + 1) / 2
+export const movingTowardsMiddle = n => snakeLengthSinceAnyCorner(n) <= middleOfSquare(n)
 
-export const calculateValueSilver = ({pos}) => Math.abs(pos.x) + Math.abs(pos.y)
+const taxiDistance = n => {
+  if (isMaxInSquare(n)) {
+    return taxiDistanceOfMaxInSquare(n)
+  }
+
+  if (movingTowardsMiddle(n)) {
+    return taxiDistanceOfMaxInSquare(n) + 2 - snakeLengthSinceAnyCorner(n)
+  } else {
+    return taxiDistanceOfMaxInSquare(n) + snakeLengthSinceAnyCorner(n) - middleOfSquare(n) - floor((widthOfSquare(n) - 3) / 2)
+  }
+}
 
 export default input$ => input$
     .map(Number)
-    .map(common({
-      calculateValue: calculateValueSilver
-    }))
-  .map(({value}) => value)
+    .map(taxiDistance)
     .flatMap(result => Bacon.once(result))
