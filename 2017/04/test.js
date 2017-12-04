8a71b458da7072dcb2223373334eaa79f203e905
Hugo Josefson <hugo@josefson.org>
2017-12-04 17:28:41 +0100

    Add input gold test.

--- 2017/04/test.js
+++ 2017/04/test.js
@@ -20,6 +20,7 @@ describe('2017-12-04', () => {
     ['abcde xyz ecdab', 0],
     ['a ab abc abd abf abj', 1],
     ['iiii oiii ooii oooi oooo', 1],
-    ['oiii ioii iioi iiio', 0]
+    ['oiii ioii iioi iiio', 0],
+    [input, 208, 'input']
   ])
 })
