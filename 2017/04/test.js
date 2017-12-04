d96ae5988da5deb094ebfe8ae0ab087df44bc248
Hugo Josefson <hugo@josefson.org>
2017-12-04 17:08:44 +0100

    Add input test.

--- 2017/04/test.js
+++ 2017/04/test.js
@@ -1,13 +1,17 @@
 /* eslint-env mocha */
 
 import describeTestCases from '../../lib/describe-test-cases'
+import inputFile from '../../lib/input-file'
 
 import silver from './silver'
+
+const input = inputFile(__dirname)
 
 describe('2017-12-04', () => {
   describeTestCases('silver', silver, [
     ['aa bb cc dd ee', 1],
     ['aa bb cc dd aa', 0],
-    ['aa bb cc dd aaa', 1]
+    ['aa bb cc dd aaa', 1],
+    [input, 386, 'input']
   ])
 })
