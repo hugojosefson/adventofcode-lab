9208ad734cb5c75dbe87fd0cd2ccb559ab85390c
Hugo Josefson <hugo@josefson.org>
2017-12-05 16:51:56 +0100

    Gold.

---
+++ 2017/05/gold.js
@@ -0,0 +1,28 @@
+import common from './common'
+
+const LOOP_DETECTOR = 100 * 1000 * 1000
+
+const cpu = jumpInstructions => {
+  let ip = 0
+  let steps = 0
+  let upperBound = jumpInstructions.length
+  while (ip < upperBound) {
+    const jumpInstruction = jumpInstructions[ip]
+    if (jumpInstruction >= 3) {
+      jumpInstructions[ip]--
+    } else {
+      jumpInstructions[ip]++
+    }
+    steps++
+    ip += jumpInstruction
+
+    if (steps > LOOP_DETECTOR) {
+      throw new Error(`I think we caught a loop. ${JSON.stringify({
+        ip, steps, upperBound, jumpInstruction
+      })}`)
+    }
+  }
+  return steps
+}
+
+export default common(cpu)
--- 2017/05/test.js
+++ 2017/05/test.js
@@ -4,6 +4,7 @@ import describeTestCases from '../../lib/describe-test-cases'
 import inputFile from '../../lib/input-file'
 
 import silver from './silver'
+import gold from './gold'
 
 const input = inputFile(__dirname)
 
@@ -15,5 +16,13 @@ describe('2017-12-05', () => {
 1
 -3`, 5],
     [input, 374269, 'input']
+  ])
+  describeTestCases('gold', gold, [
+    [`0
+3
+0
+1
+-3`, 10],
+    [input, 27720699, 'input']
   ])
 })
